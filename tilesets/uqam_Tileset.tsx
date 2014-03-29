4901189c57133862434ce181e4894be3ec7342b0
celstn <celestinmax@gmail.com>
2014-03-29 18:23:49 -0400

    ADD ageei2.tmx

---
+++ tilesets/uqam_Tileset.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE tileset SYSTEM "http://mapeditor.org/dtd/1.0/map.dtd">
+<tileset name="new_walls" tilewidth="64" tileheight="64">
+ <image source="uqam_Tileset.png" trans="ff00ff" width="1024" height="512"/>
+</tileset>
