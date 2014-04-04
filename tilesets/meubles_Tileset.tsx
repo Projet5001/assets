9a23b8796507fe6bfb9782edb71e89374f1aff18
celstn <celestinmax@gmail.com>
2014-04-04 00:06:03 -0400

    nouveau piece1_sh.tmx / dirt_tileset1.tsx / meubles_Tileset.tsx / modif de uqam_Tileset.tsx / modif sprite-Hero.png / modif de sprite-Ennemi,pgn

--- tilesets/meubles_Tileset.tsx
+++ tilesets/meubles_Tileset.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE tileset SYSTEM "http://mapeditor.org/dtd/1.0/map.dtd">
-<tileset name="uqam_Tileset" tilewidth="64" tileheight="64">
- <image source="uqam_Tileset.png" trans="ff00ff" width="1024" height="512"/>
+<tileset name="meubles_Tileset" tilewidth="64" tileheight="64">
+ <image source="meubles_Tileset.png" trans="ff00ff" width="768" height="768"/>
 </tileset>
 <!--reference tileset : http://vxresource.wordpress.com/category/resources/tilesets/-->
